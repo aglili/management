a5a275f88c4aa469c99051f4a5671d5685130b1a
aglili <cecilselorm34@gmail.com>
2023-09-01 18:22:55 +0000

    assign tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("create",views.create_task,name="create-task"),
-    path("delete/<str:task_id>",views.delete_task,name="delete_task")
+    path("delete/<str:task_id>",views.delete_task,name="delete-task"),
+    path("assign/<str:task_id>",views.assign_task,name="assign-task")
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import Task
+from accounts.models import User
 
 
 
@@ -65,6 +66,66 @@ def delete_task(request,task_id:str):
                 "error":str(e)
             }
         ,status=400)
+    
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def assign_task(request, task_id:str):
+    try:
+        task = Task.objects.get(pk=task_id)
+        assignee_username = request.data.get("assignee_username")
+
+        try:
+            assignee = User.objects.get(username=assignee_username)
+        except User.DoesNotExist:
+            return Response(
+                {
+                    "status": False,
+                    "error": "Assignee user does not exist."
+                },
+                status=404
+            )
+
+        if task.creator == request.user:
+            task.creator = assignee
+            task.save()
+
+            return Response(
+                {
+                    "status": True,
+                    "message": f"{request.user.username} assigned task '{task.title}' to '{assignee_username}'."
+                },
+                status=200
+            )
+        else:
+            return Response(
+                {
+                    "status": False,
+                    "error": "Permission denied. Only the task creator can assign the task."
+                },
+                status=403
+            )
+
+    except Task.DoesNotExist:
+        return Response(
+            {
+                "status": False,
+                "error": "Task not found."
+            },
+            status=404
+        )
+
+    except Exception as e:
+        return Response(
+            {
+                "status": False,
+                "error": str(e)
+            },
+            status=500
+        )
+
+        
+
 
     
 
