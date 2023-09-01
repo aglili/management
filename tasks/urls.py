306adb25a8d914f3412a4b3fd197045161102c6f
aglili <cecilselorm34@gmail.com>
2023-09-01 17:05:58 +0000

    delete task

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("create",views.create_task,name="create-task"),
+    path("delete/<str:task_id>",views.delete_task,name="delete_task")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from .serializers import CreateTaskSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view,permission_classes
 from rest_framework.permissions import IsAuthenticated
+from .models import Task
 
 
 
@@ -28,4 +29,45 @@ def create_task(request):
                 "error":str(e)
             }
         ,status=400)
+    
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def delete_task(request,task_id:str):
+    try:
+        task = Task.objects.get(pk=task_id)
+        if task.creator == request.user:
+            task.delete()
+            return Response(
+                {
+                    "status":True,
+                    "message":"Task Deleted"
+                }
+            ,status=200)
+        else:
+            return Response(
+                {
+                    "status":False,
+                    "error":"Permission Denied"
+                }
+            ,status=403)
+    except Task.DoesNotExist():
+        return Response(
+                {
+                    "status":False,
+                    "error":"Task Does Not Exists"
+                }
+            ,status=404)
+    except Exception as e:
+        return Response(
+            {
+                "status":False,
+                "error":str(e)
+            }
+        ,status=400)
+
+    
+
+        
+
 
