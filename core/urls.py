ddf5f8daccb4a3f0d535ea95a058522cb21da533
aglili <cecilselorm34@gmail.com>
2023-09-11 22:28:53 +0000

    new changes

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API",
+        title="OTMS- Online Task Management System",
         default_version="v1",
         description="Your API description",
         terms_of_service="https://www.yourapp.com/terms/",
